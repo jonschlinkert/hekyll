1afee8ce9bc91f81a5239e4fbf100fd6681f1933
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-15 17:07:14 -0400

    example

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+
+var gulp = require('gulp');
+var vfs = require('vinyl-fs');
+var convert = require('gulp-liquid-to-handlebars');
+
+require('./')(gulp, {
+  task: 'hekyll',
+  theme: 'poole',
+  themes: 'vendor',
+  dest: 'tmp/src'
+});
+
+gulp.task('convert', function() {
+  return vfs.src('**/*.*', { cwd: 'vendor/shopify/skeleton' })
+    .pipe(convert())
+    .pipe(gulp.dest(function(file) {
+      if (/\.(liquid|html)/.test(file.extname)) {
+        file.extname = '.hbs';
+      }
+      return 'tmp/src';
+    }))
+});
+
+gulp.task('default', ['hekyll']);
